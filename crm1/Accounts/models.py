2ae94a9408533d9d58b584fcde767fcc973d07c2
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-03-10 23:28:04 +0530

    adding dynamic data

--- crm1/Accounts/models.py
+++ crm1/Accounts/models.py
@@ -6,4 +6,45 @@ class Customer(models.Model):
     phone= models.CharField(max_length=10)
     email =models.EmailField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
     
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    CATEGORY = {
+        ('Indore', 'Indore'),
+        ('Out Door','Out Door'),
+    }
+    name = models.CharField(max_length=200)
+    price =models.FloatField(null=True)
+    category = models.CharField(max_length=200, choices=CATEGORY)
+    description = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+    
+
+
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, null=True ,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True, on_delete= models.SET_NULL)
+    STATUS = {
+        ('Pending','Pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivered','Delivered')
+    }
+
+    date_created =models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+    def __str__(self):
+        return self.status
--- crm1/Accounts/views.py
+++ crm1/Accounts/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'Accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status ='Delivered').count()
+    pending = orders.filter(status ='Pending').count()
+    context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'total_customers':total_customers,'delivered':delivered,'pending':pending}
+    return render(request, 'Accounts/dashboard.html' ,context)
     #return HttpResponse("Hello, Account App page Django!")
 
-def customer(request):
-    return render(request, 'Accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+
+    context = {'customer':customer, 'orders':orders}
+    return render(request, 'Accounts/customer.html',context)
     #return HttpResponse(" Account app Contact Page")
 
 
 def products(request):
-    return render(request, 'Accounts/products.html')
+    product = Product.objects.all()
+    return render(request, 'Accounts/products.html', {'product':product})
