7b3e137c274b013e86345475063525792acbb1ed
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-03-10 17:06:32 +0530

    create Customer Model

--- crm1/Accounts/models.py
+++ crm1/Accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone= models.CharField(max_length=10)
+    email =models.EmailField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
