5c30381910dd1cda3cb31453ddef3d4746865587
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-03-14 14:23:04 +0530

    passowrd rest via email is done

--- crm1/Accounts/urls.py
+++ crm1/Accounts/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.urls import path
 from django.http import HttpResponse
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    
@@ -34,5 +34,22 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('user/', views.userPage, name='user'),
     path('account/',views.accountSetting, name='account'),
+
+    path('reset_password/',
+          auth_views.PasswordResetView.as_view(template_name="Accounts/password_reset.html")
+         ,name='reset_password'),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name="Accounts/password_reset_sent.html"),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name="Accounts/password_reset_form.html"),
+           name= 'password_reset_confirm'),
+
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name="Accounts/password_reset_done.html"),
+         name='password_reset_complete'),
+
+
 
 ] 
--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -134,3 +134,13 @@ MEDIA_URL = '/images/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#smtp configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pankajbadgujar799@gmail.com' 
+EMAIL_HOST_PASSWORD = 'ttne trjf cuxg bmtc'
